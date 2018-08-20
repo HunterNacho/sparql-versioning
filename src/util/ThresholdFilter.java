dbaa101bd6289b2e08aadc3a0901118ccb5e0aa8
Ignacio Cuevas <ignacio.cuevas@ug.uchile.cl>
2018-08-20 18:55:04 -0300

    pre-compiled pattern to improve performance

--- src/util/ThresholdFilter.java
+++ src/util/ThresholdFilter.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.io.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
@@ -8,10 +9,12 @@ import java.util.zip.GZIPOutputStream;
 public class ThresholdFilter {
     private static final String outputFolder = "filtered/";
     private static final int threshold = 100000;
+    private static final Pattern pattern = Pattern.compile("Q\\d+>");
 
     private static int getIdNumber(String entity) {
         assert entity.startsWith("<") && entity.endsWith(">");
-        if (!Pattern.matches("Q\\d+>", entity))
+        Matcher matcher = pattern.matcher(entity);
+        if (!matcher.matches())
             return -1;
         int index = entity.lastIndexOf("Q");
         entity = entity.substring(index + 1, entity.length() - 1);
