924088786e2201ac899ad010e2ed9ad216cdbba6
Ignacio Cuevas <ignacio.cuevas@ug.uchile.cl>
2018-08-23 12:54:31 -0300

    removed regular expression due to performance issues

--- src/util/ThresholdFilter.java
+++ src/util/ThresholdFilter.java
@@ -1,24 +1,27 @@
 package util;
 
 import java.io.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 public class ThresholdFilter {
     private static final String outputFolder = "filtered/";
     private static final int threshold = 100000;
-    private static final Pattern pattern = Pattern.compile("Q\\d+>");
 
     private static int getIdNumber(String entity) {
         assert entity.startsWith("<") && entity.endsWith(">");
-        Matcher matcher = pattern.matcher(entity);
-        if (!matcher.matches())
-            return -1;
         int index = entity.lastIndexOf("Q");
+        if (index < 0)
+            return index;
         entity = entity.substring(index + 1, entity.length() - 1);
-        return Integer.parseInt(entity);
+        int number;
+        try {
+            number = Integer.parseInt(entity);
+        }
+        catch (NumberFormatException e) {
+            number = -1;
+        }
+        return number;
     }
 
     public static void main(String[] args) throws IOException{
