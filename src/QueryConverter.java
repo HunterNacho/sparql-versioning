6a886a2eb59406a3a88ff3d2c2351381d024c665
Ignacio Cuevas <ignacio.cuevas@ug.uchile.cl>
2018-08-26 21:47:23 -0300

    added files that adapt queries for versioned graphs

---
+++ src/QueryConverter.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class QueryConverter {
+    private static final String baseFolder = "/home/icuevas/scripts/queries/wrapped/";
+    private static final String[] graphs = new String[]{
+            "filtered-20170830",
+            "filtered-20170907",
+            "filtered-20170913",
+            "filtered-20170920",
+            "filtered-20170927"
+    };
+    public static void main (String[] args) throws IOException{
+        if (args.length < 1) {
+            System.err.println("Password not provided");
+            System.exit(1);
+            return;
+        }
+        String password =  args[0];
+        String scriptHeader = "isql-vt 1111 dba " + password + " exec=\"load ";
+        String scriptTail = ";\" > results/";
+        BufferedWriter script = new BufferedWriter(new FileWriter(baseFolder + "exec-all-queries"));
+        for (int i = 0; i < 385; i++) {
+            String queryId = "q" + String.format("%03d", i);
+            String filename = baseFolder + queryId + ".sql";
+            QueryHandler query;
+            try {
+                query = new QueryHandler(filename);
+            }
+            catch (FileNotFoundException e) {
+                System.err.println("Skipping file " + queryId + ".sql");
+                continue;
+            }
+            for (int j = 0; j < 5; j++) {
+                String currentGraph = graphs[j];
+                script.write(scriptHeader + filename + scriptTail + currentGraph + "_" + queryId);
+                script.newLine();
+                String output = baseFolder + currentGraph + queryId + ".sql";
+                query.insertGraphAndWrite("http://wikidata.org/" + currentGraph, output);
+            }
+        }
+        script.close();
+    }
+}
---
+++ src/QueryHandler.java
@@ -0,0 +1,102 @@
+import java.io.*;
+import java.util.ArrayList;
+
+class QueryHandler {
+    private ArrayList<String> lines = new ArrayList<>();
+    private int openingBraceLineIndex;
+    private int openingBraceCharIndex;
+    private int closingBraceLineIndex;
+    private int closingBraceCharIndex;
+    QueryHandler(String filename) throws IOException {
+        BufferedReader input = new BufferedReader(new FileReader(filename));
+        String line;
+        while ((line = input.readLine()) != null) {
+            line = line.trim();
+            lines.add(line);
+        }
+        input.close();
+        findOpeningBraceIndexes();
+        findClosingBraceIndexes();
+    }
+
+    private void findOpeningBraceIndexes() {
+        boolean whereFound = false;
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            if (line.contains("WHERE")) {
+                whereFound = true;
+            }
+            if (whereFound && line.contains("{")) {
+                if (line.lastIndexOf("{") < line.indexOf("WHERE"))
+                    continue;
+                openingBraceLineIndex = i;
+                openingBraceCharIndex = line.indexOf("{", line.contains("WHERE") ? line.indexOf("WHERE") : 0);
+                return;
+            }
+        }
+    }
+
+    private void findClosingBraceIndexes() {
+        int braces = 1;
+        for (int i = openingBraceLineIndex; i < lines.size(); i++) {
+            String toCheck = lines.get(i);
+            if (i == openingBraceLineIndex) {
+                toCheck = toCheck.substring(openingBraceCharIndex + 1);
+            }
+            for (int j = 0; j < toCheck.length(); j++) {
+                switch (toCheck.charAt(j)) {
+                    case '{':
+                        braces++;
+                        break;
+                    case '}':
+                        braces--;
+                        break;
+                    default:
+                        break;
+                }
+                if (braces == 0) {
+                    closingBraceLineIndex = i;
+                    closingBraceCharIndex = j;
+                    return;
+                }
+            }
+        }
+    }
+
+    void insertGraphAndWrite(String graphName, String outputFilename) throws IOException {
+        BufferedWriter output = new BufferedWriter(new FileWriter(outputFilename));
+        int i;
+        for (i = 0; i < lines.size(); i++) {
+            if (i == openingBraceLineIndex && i == closingBraceLineIndex) {
+                String line = lines.get(i);
+                output.write(
+                        line.substring(0, openingBraceCharIndex + 1)
+                        + " GRAPH <" + graphName + "> {"
+                        + line.substring(openingBraceCharIndex + 1, closingBraceCharIndex)
+                        + "}" + line.substring(closingBraceCharIndex)
+                );
+                output.newLine();
+                continue;
+            }
+            if (i == openingBraceLineIndex) {
+                String line = lines.get(i);
+                output.write(
+                        line.substring(0, openingBraceCharIndex + 1)
+                        + " GRAPH <" + graphName + "> {"
+                        + line.substring(openingBraceCharIndex + 1)
+                );
+                output.newLine();
+                continue;
+            }
+            if (i == closingBraceLineIndex) {
+                String line = lines.get(i);
+                output.write(line.substring(0, closingBraceCharIndex) + "}" + line.substring(closingBraceCharIndex));
+                output.newLine();
+                continue;
+            }
+            output.write(lines.get(i));
+            output.newLine();
+        }
+        output.close();
+    }
+}
