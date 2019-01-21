a4d369bd4251f95195cf3fd7434d3439fa768684
Ignacio Cuevas <ignacio.cuevas@ug.uchile.cl>
2019-01-21 20:28:57 -0300

    added query results csv builder

---
+++ src/QueryResultsCsvBuilder.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class QueryResultsCsvBuilder {
+    public static void main(String[] args) throws IOException {
+        File folder = new File(args[0]);
+        File[] files = folder.listFiles();
+        assert files != null;
+        BufferedWriter writer = new BufferedWriter(new FileWriter("results.csv"));
+        for (File file : files) {
+            if (file.isDirectory() || !file.getName().contains(".rq")) {
+                continue;
+            }
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String stats = "N/A";
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                if (line.contains("msec."))
+                    stats = line;
+            }
+            writer.write(file.getName() + ", " + stats);
+            reader.close();
+        }
+        writer.close();
+    }
+}
