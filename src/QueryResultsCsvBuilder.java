e46616cdf52195c2d8f9e82268a689872517cade
Ignacio Cuevas <ignacio.cuevas@ug.uchile.cl>
2019-01-22 12:27:36 -0300

    refined output

--- src/QueryResultsCsvBuilder.java
+++ src/QueryResultsCsvBuilder.java
@@ -6,17 +6,26 @@ public class QueryResultsCsvBuilder {
         File[] files = folder.listFiles();
         assert files != null;
         BufferedWriter writer = new BufferedWriter(new FileWriter("results.csv"));
+        writer.write("query,rows,time");
+        writer.newLine();
         for (File file : files) {
             if (file.isDirectory() || !file.getName().contains(".rq")) {
                 continue;
             }
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            String stats = "N/A";
+            String stats = "";
             for (String line = reader.readLine(); line != null; line = reader.readLine()) {
                 if (line.contains("msec."))
                     stats = line;
             }
-            writer.write(file.getName() + ", " + stats);
+            int rows = 0;
+            long time = 0;
+            if (stats != "") {
+                String[] split = stats.split("--");
+                rows = Integer.parseInt(split[0].replace("Rows.", "").trim());
+                time = Long.parseLong(split[1].replace("msec.", "").trim());
+            }
+            writer.write(file.getName() + "," + rows + "," + time);
             writer.newLine();
             reader.close();
         }
