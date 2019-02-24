1db9fa29c0416f96574f46f6eab6031045cdc162
Ignacio Cuevas <ignacio.cuevas@ug.uchile.cl>
2019-02-24 12:20:07 -0300

    added script that fixes unicode escaping in queries

---
+++ src/QueryUnicodeEscape.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class QueryUnicodeEscape {
+    public static void main(String[] args) throws IOException{
+        File folder = new File(args[0]);
+        assert folder.isDirectory();
+        File[] files = folder.listFiles();
+        assert files != null;
+        for (File file : files) {
+            if (file.isDirectory() || !file.getName().contains(".rq"))
+                continue;
+            BufferedWriter writer = new BufferedWriter(new FileWriter("clean/" + file.getName()));
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                writer.write(line.replace("\\u", "\\\\u"));
+            }
+            writer.close();
+            reader.close();
+        }
+    }
+}
